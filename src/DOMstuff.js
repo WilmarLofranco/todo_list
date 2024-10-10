518d2e330a991f145c0cc9fae08b097fe5abe61a
WilmarLofranco <wilmarlofranco@gmail.com>
2024-10-10 21:33:25 +0800

    Add DOM manipulation in project creation

--- src/DOMstuff.js
+++ src/DOMstuff.js
@@ -1,26 +1,33 @@
 import {projects, Project, Task} from "./appLogic.js";
 
 const modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 const btn = document.querySelector(".sidebar > button");
-
-// Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target === modal) {
         modal.style.display = "none";
     }
+}
+
+
+
+
+
+document.getElementById("myForm").onsubmit = function (event) {
+    event.preventDefault();
+    const projTitle = document.getElementById("projTitle").value;
+    const projectTitle = document.querySelector(".main > div > h2");
+    const newProject = new Project (projTitle);
+    projectTitle.textContent = `Project: ${projTitle}`;
+    document.getElementById("myForm").reset();
+    modal.style.display = "none";
 }
